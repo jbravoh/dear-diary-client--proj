ab90fb24657720c239de63ee6e4db04a1fb52470
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-02 21:50:06 +0100

    Add navbar and navbar css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
+import "../css/Navbar.css";
 
 export default function Navbar(): JSX.Element {
   return (
-    <div>
-      <nav>
-        <h1><Link to="/">Dear Diary</Link></h1>
-        <div>
-          <ul>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/new-post">New Post</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar-items">
+      <h1 className="navbar-logo ">
+        <Link to="/" className="link">
+          Dear Diary
+        </Link>
+      </h1>
+
+      <div className="nav-container">
+        <ul className="nav-menu">
+          <li>
+            <Link to="/new-post" className="nav-link">
+              New Post
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="nav-link">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
