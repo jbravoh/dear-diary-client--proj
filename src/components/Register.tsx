eb95b40220b12899fd1eb66d4c44342e1501e6a9
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-02 21:49:28 +0100

    Completed form structure and css

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,23 +1,25 @@
 import style from "../css/Forms.module.css";
 import { Link } from "react-router-dom";
 
-export default function Login(): JSX.Element {
+export default function Register(): JSX.Element {
   return (
-    <>
-      <h1 className="title">Login</h1>
-      <form className={style.formContainer}>
+    <div className={style.formContainer}>
+      <h1 className="title">Register</h1>
+      <form>
+        <label className={style.label}>Username</label>
+        <input type="text" name="username" placeholder="Enter username"></input>
         <label className={style.label}>Email</label>
         <input type="text" name="email" placeholder="Enter email"></input>
         <label className={style.label}>Password</label>
         <input type="text" name="password" placeholder="Enter password"></input>
-        <button className={style.button}>Login</button>
+        <button className={style.button}>Create Account</button>
       </form>
       <p className={style.formSentence}>
         Already have an account?{" "}
-        <Link to="/register" className={style.formLink}>
-          Join Now
+        <Link to="/login" className={style.formLink}>
+          Login
         </Link>
       </p>
-    </>
+    </div>
   );
 }
