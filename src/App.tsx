f5e713a0c9b38b47a596732d848f989f680ec2c3
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-04 12:18:50 +0100

    Pass setAuth to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -16,23 +16,34 @@ function App(): JSX.Element {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   const setAuth = (boolean: boolean) => {
-    setIsAuthenticated(boolean)
-  }
-
+    setIsAuthenticated(boolean);
+  };
 
   return (
     <div className="app-container">
       <Router>
-        <Navbar />
+        <Navbar setAuth={setAuth} />
         <Switch>
           <Route exact path="/dashboard">
-            {isAuthenticated ? <Dashboard setAuth={setAuth}/> : <Redirect to="/login" />}
+            {isAuthenticated ? (
+              <Dashboard setAuth={setAuth} />
+            ) : (
+              <Redirect to="/login" />
+            )}
           </Route>
           <Route path="/login">
-            {!isAuthenticated ? <Login setAuth={setAuth} /> : <Redirect to="/dashboard" />}
+            {!isAuthenticated ? (
+              <Login setAuth={setAuth} />
+            ) : (
+              <Redirect to="/dashboard" />
+            )}
           </Route>
           <Route path="/register">
-            {!isAuthenticated ? <Register setAuth={setAuth}/> : <Redirect to="/login" />}
+            {!isAuthenticated ? (
+              <Register setAuth={setAuth} />
+            ) : (
+              <Redirect to="/login" />
+            )}
           </Route>
           <Route path="/new-post">
             <NewPost />
