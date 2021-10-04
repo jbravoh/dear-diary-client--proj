98f1c7db4785deffd8f78ac3acd76709b5305e0c
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-04 11:30:33 +0100

    Add authentication variable and setAuth function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./css/App.css";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -8,22 +13,29 @@ import NewPost from "./components/NewPost";
 import Dashboard from "./components/Dashboard";
 
 function App(): JSX.Element {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const setAuth = (boolean: boolean) => {
+    setIsAuthenticated(boolean)
+  }
+
+
   return (
     <div className="app-container">
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/">
-            <Dashboard />
+          <Route exact path="/dashboard">
+            {isAuthenticated ? <Dashboard setAuth={setAuth}/> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/login">
+            {!isAuthenticated ? <Login setAuth={setAuth} /> : <Redirect to="/dashboard" />}
+          </Route>
+          <Route path="/register">
+            {!isAuthenticated ? <Register setAuth={setAuth}/> : <Redirect to="/login" />}
           </Route>
           <Route path="/new-post">
             <NewPost />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
           </Route>
         </Switch>
       </Router>
