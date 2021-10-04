b4253f077bbc3c7bb2b9d2808b31c1887fb26683
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-04 13:24:34 +0100

    Add conditional rendering to nav depending on auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,38 +3,31 @@ import "../css/Navbar.css";
 
 interface NavbarProps {
   setAuth: (boolean: boolean) => void;
+  isAuthenticated: boolean;
 }
 
-export default function Navbar({ setAuth }: NavbarProps): JSX.Element {
+export default function Navbar({
+  setAuth,
+  isAuthenticated,
+}: NavbarProps): JSX.Element {
   const logout = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     localStorage.removeItem("token");
     setAuth(false);
   };
 
-  return (
-    <nav className="navbar-items">
-      <h1 className="navbar-logo ">
-        <Link to="/dashboard" className="link">
-          Dear Diary
-        </Link>
-      </h1>
-
+  const authenticatedNav = () => {
+    return (
       <div className="nav-container">
         <ul className="nav-menu">
           <li>
+            <Link to="/dashboard" className="nav-link">
+              Dashboard
+            </Link>
+          </li>
+          <li>
             <Link to="/new-post" className="nav-link">
               New Post
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link to="/register" className="nav-link">
-              Register
             </Link>
           </li>
           <li>
@@ -48,6 +41,36 @@ export default function Navbar({ setAuth }: NavbarProps): JSX.Element {
           </li>
         </ul>
       </div>
+    );
+  };
+
+  const notAuthenticatedNav = () => {
+    return (
+      <div className="nav-container">
+        <ul className="nav-menu">
+          <li>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="nav-link">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <nav className="navbar-items">
+      <h1 className="navbar-logo ">
+        <Link to="/dashboard" className="link">
+          Dear Diary
+        </Link>
+      </h1>
+      {isAuthenticated ? authenticatedNav() : notAuthenticatedNav()}
     </nav>
   );
 }
