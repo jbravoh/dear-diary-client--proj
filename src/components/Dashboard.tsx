1ee37f819f691c4633c9e991911e70af4bd34e09
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-04 12:17:36 +0100

    Build Dashboard component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,3 +1,32 @@
-export default function Dashboard(): JSX.Element {
-  return <h1 className="title">Welcome</h1>;
+import React, { useState, useEffect } from "react";
+
+interface DashboardProps {
+  setAuth: (boolean: boolean) => void;
+}
+export default function Dashboard({ setAuth }: DashboardProps): JSX.Element {
+  const [name, setName] = useState<string>("");
+
+  const getName = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+      setName(parseRes.username);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getName();
+  });
+
+  return (
+    <>
+      <h1 className="title">Welcome {name}</h1>
+    </>
+  );
 }
