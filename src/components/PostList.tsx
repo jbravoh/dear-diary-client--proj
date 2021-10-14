e0f249e19f51d0654a284d82fb6c7baa7af8c7f6
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-14 14:54:49 +0100

    Build SearchInput component and apply css

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IPost } from "../interfaces/IPost";
 import style from "../css/PostList.module.css";
 import { Link } from "react-router-dom";
 import TimeAgo from "react-timeago";
+import SearchInput from "./SearchInput";
 
 interface PostListProps {
   allPosts: IPost[];
@@ -15,6 +16,8 @@ export default function PostList({
   posts,
   setPosts,
 }: PostListProps): JSX.Element {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   useEffect(() => {
     setPosts(allPosts);
   }, [allPosts, setPosts]);
@@ -22,34 +25,42 @@ export default function PostList({
   const displayPosts = () => {
     return (
       <div className={style.container}>
-        {posts.map((post) => (
-          <div key={post.post_id} className={style.postContainer}>
-            <h1 className={style.title}> {post.title}</h1>
-            {post.created !== undefined && <TimeAgo date={post.created} />}
-
-            <div key={post.post_id}>
-              <Link
-                to={`/selected-post/${post.post_id}`}
-                className={style.buttonLink}
-              >
-                <button className={style.button}>View</button>
-              </Link>
+        {posts
+          .filter(
+            (post) =>
+              post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              post.content.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((post) => (
+            <div key={post.post_id} className={style.postContainer}>
+              <h1 className={style.title}> {post.title}</h1>
+              {post.created !== undefined && <TimeAgo date={post.created} />}
+              <div key={post.post_id}>
+                <Link
+                  to={`/selected-post/${post.post_id}`}
+                  className={style.buttonLink}
+                >
+                  <button className={style.button}>View</button>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     );
   };
 
   return (
-    <div>
+    <>
       {allPosts.length === 1 && allPosts[0].post_id === null ? (
         <Link to={"/new-post"} className={style.buttonLink}>
           <button className={style.newPostButton}>Create New Post</button>{" "}
         </Link>
       ) : (
-        displayPosts()
+        <>
+          <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          {displayPosts()}
+        </>
       )}
-    </div>
+    </>
   );
 }
